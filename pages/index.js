c1982b4d57f62ceeb0e2f65581b66d2b8ce4c7c5
Nicolás Yagüe <nyaguenasif@gmail.com>
2023-05-09 20:56:26 -0300

    new commit

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="w-full h-screen flex flex-col gap-2 justify-center items-center bg-black text-white text-xl">
       <h1 className="text-5xl font-oswald">CYBER ZONE</h1>
-      <h2 className="text-2xl font-oswald">Coming Soon</h2>
+      <h2 className="text-2xl font-oswald">Coming Soon...</h2>
     </div>
   );
 }
