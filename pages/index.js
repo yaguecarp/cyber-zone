418a4ad875387a1308007ff5b3cef1925889659c
Nicolás Yagüe <nyaguenasif@gmail.com>
2023-05-09 20:41:27 -0300

    new commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-black text-white text-xl">
-      <h1 className="text-5xl body-font font-roboto">Cyber Zone</h1>
+    <div className="w-full h-screen flex flex-col gap-2 justify-center items-center bg-black text-white text-xl">
+      <h1 className="text-5xl font-oswald">CYBER ZONE</h1>
+      <h2 className="text-2xl font-oswald">Coming Soon</h2>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'roboto': ['Roboto', 'sans-serif'],
+        'oswald': ['Oswald', 'sans-serif'],
       },
     },
   },
