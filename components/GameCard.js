dedf4debdd9ae461cfd0d83506361b6f09fa1305
Nicolás Yagüe <nyaguenasif@gmail.com>
2023-06-27 22:26:00 -0300

    as

--- components/GameCard.js
+++ components/GameCard.js
@@ -4,7 +4,6 @@ export default function GameCard({ imagen, titulo, plataformas }) {
       <div className="card">
         <img src={imagen} alt="" className="w-full h-full object-cover" />
         <div className="p-2 flex w-full  justify-start items-center ">
-          {/* {plataformas} */}
           <h2 className="text-lg  ">{titulo}</h2>
         </div>
       </div>
